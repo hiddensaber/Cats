374058518d612d2fa05bbee9c45a60edd7c349e1
Alex <Alex@142.232.58.24>
2016-01-24 12:07:55 -0800

    lab 2 part 3 complete

--- application/models/Images.php
+++ application/models/Images.php
@@ -13,4 +13,12 @@ class Images extends CI_Model {
         $query = $this->db->get('images');
         return $query->result_array();
     }
+    
+    function newest()
+    {
+        $this->db->order_by("id", "desc");
+        $this->db->limit(3);
+        $query = $this->db->get('images');
+        return $query->result_array();
+    }
 }
